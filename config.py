8804b5fd6448522209a9855ab6d5223e5df3d461
unknown <camgmanzur@gmail.com>
2026-05-07 14:49:28 -0300

    cambiaron la direccion de bullmarket

--- config.py
+++ config.py
@@ -7,9 +7,13 @@ load_dotenv()
 BMB_USERNAME = os.getenv("BMB_USERNAME")
 BMB_PASSWORD = os.getenv("BMB_PASSWORD")
 CHROMEDRIVER_PATH = os.getenv("CHROMEDRIVER_PATH")
-LOGIN_URL = "https://bullmarketbrokers.com/Security/SignIn"
-DASHBOARD_URL = "https://www.bullmarketbrokers.com/Clients/Dashboard"
-CAUCIONES_URL = "https://www.bullmarketbrokers.com/Cotizaciones/cauciones"
+LOGIN_URL = "https://inversiones.bullmarket.com.ar/Security/SignIn"
+# "https://bullmarketbrokers.com/Security/SignIn"
+DASHBOARD_URL = "https://inversiones.bullmarket.com.ar/clients/dashboard/"
+# "https://www.bullmarketbrokers.com/Clients/Dashboard"
+CAUCIONES_URL = "https://inversiones.bullmarket.com.ar/Cotizaciones/cauciones"
+# "https://www.bullmarketbrokers.com/Cotizaciones/cauciones"
+
 
 
 # Configuración de almacenamiento HDF5
@@ -19,16 +23,16 @@ ACCUMULATE = False  # True para acumular en archivo histórico, False para solo 
 
 # URLs de extracción
 URLS = [
-    "https://www.bullmarketbrokers.com/Cotizaciones/merval",
-    "https://www.bullmarketbrokers.com/Cotizaciones/panel%20general",
-    "https://www.bullmarketbrokers.com/Cotizaciones/cedears",
-    "https://www.bullmarketbrokers.com/Cotizaciones/bonos",
-    "https://www.bullmarketbrokers.com/Cotizaciones/opciones",
-    "https://www.bullmarketbrokers.com/Cotizaciones/letras",
-    "https://www.bullmarketbrokers.com/Cotizaciones/rofex",
-    "https://www.bullmarketbrokers.com/Cotizaciones/licitaciones",
-    "https://www.bullmarketbrokers.com/Cotizaciones/fondos",
-    "https://www.bullmarketbrokers.com/Cotizaciones/cauciones",
-    "https://www.bullmarketbrokers.com/Cotizaciones/obligacionesNegociables",
-   "https://www.bullmarketbrokers.com/Cotizaciones/US"
+    "https://inversiones.bullmarket.com.ar/Cotizaciones/merval",
+    "https://inversiones.bullmarket.com.ar/Cotizaciones/panel%20general",
+    "https://inversiones.bullmarket.com.ar/Cotizaciones/cedears",
+    "https://inversiones.bullmarket.com.ar/Cotizaciones/bonos",
+    "https://inversiones.bullmarket.com.ar/Cotizaciones/opciones",
+    "https://inversiones.bullmarket.com.ar/Cotizaciones/letras",
+    "https://inversiones.bullmarket.com.ar/Cotizaciones/rofex",
+    "https://inversiones.bullmarket.com.ar/Cotizaciones/licitaciones",
+    "https://inversiones.bullmarket.com.ar/Cotizaciones/fondos",
+    "https://inversiones.bullmarket.com.ar/Cotizaciones/cauciones",
+    "https://inversiones.bullmarket.com.ar/Cotizaciones/obligacionesNegociables",
+    "https://inversiones.bullmarket.com.ar/Cotizaciones/US"
 ]
